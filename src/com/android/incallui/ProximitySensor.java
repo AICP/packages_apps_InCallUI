6c426c432383b413413e3aa08771273b07905dd0
eyosen <abittin@gmail.com>
2015-07-30 12:06:02 +0300

    Revert "Disable Touch by Proximity Sensor"
    This reverts commit e97a919f116efe39cc6e48d949d868180a98dd98.
    
    Change-Id: I29b98da03052d5c8c0e6cf963784727a5339f9a3

--- src/com/android/incallui/ProximitySensor.java
+++ src/com/android/incallui/ProximitySensor.java
@@ -51,7 +51,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
     private Sensor mProxSensor;
     private final AudioModeProvider mAudioModeProvider;
     private final AccelerometerListener mAccelerometerListener;
-    private final ProximityListener mProximityListener;
     private int mOrientation = AccelerometerListener.ORIENTATION_UNKNOWN;
     private boolean mUiShowing = false;
     private boolean mIsPhoneOutgoing = false;
@@ -86,7 +85,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
         }
 
         mAccelerometerListener = new AccelerometerListener(mContext, this);
-        mProximityListener = new ProximityListener(context);
         mAudioModeProvider = audioModeProvider;
         mAudioModeProvider.addListener(this);
     }
@@ -95,7 +93,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
         mAudioModeProvider.removeListener(this);
 
         mAccelerometerListener.enable(false);
-        mProximityListener.enable(false);
 
         TelecomAdapter.getInstance().turnOffProximitySensor(true);
 
@@ -139,7 +136,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
 
             mOrientation = AccelerometerListener.ORIENTATION_UNKNOWN;
             mAccelerometerListener.enable(mIsPhoneOffhook);
-            mProximityListener.enable(mIsPhoneOffhook);
 
             updateProxSpeaker();
             updateProximitySensorMode();
@@ -219,10 +215,6 @@ public class ProximitySensor implements AccelerometerListener.ChangeListener,
      */
     public boolean isScreenReallyOff() {
         return !mPowerManager.isScreenOn();
-    }
-
-    public boolean isScreenOffByProximity() {
-        return mProximityListener.isActive();
     }
 
     /**
